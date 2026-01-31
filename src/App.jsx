ed20a8af96c77f691cad8c2a42e9cfe7ec277025
Flanelie <ahmadrafimusyaffa@gmail.com>
2026-01-31 13:28:32 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -411,7 +411,7 @@ const UTBKStudentApp = () => {
     else { if (currentSubtestIndex < testOrder.length - 1) { setScreen('break'); setBreakTime(10); } else { setScreen('result'); } }
   };
 
-  const FooterLiezira = () => (<div className="mt-8 py-4 border-t border-gray-200 w-full text-center"><p className="text-gray-400 text-xs font-mono flex items-center justify-center gap-1"><Copyright size={12} /> {new Date().getFullYear()} Created by <span className="font-bold text-indigo-400">Liezira</span></p></div>);
+  const FooterLiezira = () => (<div className="mt-8 py-4 border-t border-gray-200 w-full text-center"><p className="text-gray-400 text-xs font-mono flex items-center justify-center gap-1"><Copyright size={12} /> {new Date().getFullYear()} Created by <span className="font-bold text-indigo-400">RuangSimulasi</span></p></div>);
 
   // --- ANALYSIS DASHBOARD (Responsive & Optimized for Mobile) ---
   const AnalysisDashboard = () => {
