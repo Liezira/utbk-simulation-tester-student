0bd1d389ce51563adb2d4f2f34ed26752f5be866
Flanelie <ahmadrafimusyaffa@gmail.com>
2026-02-03 17:09:32 +0700

    updateSoalPK

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const SUBTESTS = [
   { id: 'pu', name: 'Penalaran Umum', questions: 30, time: 30 },
   { id: 'ppu', name: 'Pengetahuan & Pemahaman Umum', questions: 20, time: 15 },
   { id: 'pbm', name: 'Pemahaman Bacaan & Menulis', questions: 20, time: 25 },
-  { id: 'pk', name: 'Pengetahuan Kuantitatif', questions: 15, time: 20 },
+  { id: 'pk', name: 'Pengetahuan Kuantitatif', questions: 20, time: 20 },
   { id: 'lbi', name: 'Literasi Bahasa Indonesia', questions: 30, time: 45 },
   { id: 'lbe', name: 'Literasi Bahasa Inggris', questions: 20, time: 30 },
   { id: 'pm', name: 'Penalaran Matematika', questions: 20, time: 30 },
